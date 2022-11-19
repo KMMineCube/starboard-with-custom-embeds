25f18188d4ff13e491398f4e3142b7196561f130
KMMineCube <107238489+KMMineCube@users.noreply.github.com>
2022-11-18 21:06:47 -0800

    Generalized message_to_embeds.ts functions

--- src/message_to_embed.ts
+++ src/message_to_embed.ts
@@ -1,5 +1,18 @@
-import { Message } from 'discord.js';
+import { Collection, Message } from 'discord.js';
 import { composeRedditEmbed, searchForRedditLink } from './reddit_stuff.js';
+import { composeCustomEmbedCallback, searchForLinkCallback } from './utilities.js';
+
+const searchAndEmbedCollection: {
+    type: string;
+    searchFunction: searchForLinkCallback;
+    composeEmbedFunction: composeCustomEmbedCallback;
+}[] = [
+    {
+        type: 'reddit',
+        searchFunction: searchForRedditLink,
+        composeEmbedFunction: composeRedditEmbed
+    }
+];
 
 /**
  * Function to replace a user-sent message that contains a media link with a custom embed
@@ -11,16 +24,29 @@ async function replaceLinkWithEmbed(message: Message): Promise<void> {
     const content = message.content;
     const channel = message.channel;
 
-    const redditLinks = await searchForRedditLink(content);
+    const searchResults = await Promise.all(
+        searchAndEmbedCollection.map(async (obj) => {
+            const links = await obj.searchFunction(content);
+            return { links, obj };
+        })
+    );
 
-    const redditLink = redditLinks.length === 1 ? redditLinks[0] : null;
+    const numLinks = searchResults.reduce((acc, val) => acc + val.links.length, 0);
 
-    if (redditLink) {
+    if (numLinks !== 1) return;
+
+    // get the first link that is found
+    const { links, obj } = searchResults.find((val) => val.links.length > 0) ?? {
+        links: [],
+        obj: undefined
+    };
+
+    if (links && links[0] && obj) {
         // delete original message
         await message.delete();
         // send reddit link to server
         await channel.send(
-            await composeRedditEmbed(redditLink, content, message.member?.user)
+            await obj.composeEmbedFunction(links[0], content, message.member.user)
         );
     }
 }
--- src/utilities.ts
+++ src/utilities.ts
@@ -1,5 +1,16 @@
+import { BaseMessageOptions, User } from 'discord.js';
+
 function notEmpty<TValue>(value: TValue | null | undefined): value is TValue {
     return value !== null && value !== undefined;
 }
 
-export { notEmpty };
+type searchForLinkCallback = (link: string) => Promise<string[]>;
+
+type composeCustomEmbedCallback = (
+    link: string,
+    message: string,
+    sender: User,
+    pageNumber?: number
+) => Promise<BaseMessageOptions>;
+
+export { notEmpty, searchForLinkCallback, composeCustomEmbedCallback };
